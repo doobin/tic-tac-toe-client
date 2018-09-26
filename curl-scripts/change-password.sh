875cca056c4e29335c00d89e09621e847c2279b2
doobin <doobin42@gmail.com>
2018-09-26 11:29:44 -0400

    Version_1
    -Added New development URL(https://tic-tac-toe-wdi.herokuapp.com/) to config.js, curl-scripts

--- curl-scripts/change-password.sh
+++ curl-scripts/change-password.sh
@@ -1,6 +1,6 @@
 # ID=2 sh curl-scripts/json/change-password.sh
 
-curl "http://tic-tac-toe.wdibos.com/change-password" \
+curl "https://tic-tac-toe-wdi.herokuapp.com/change-password/" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
--- curl-scripts/sign-up.sh
+++ curl-scripts/sign-up.sh
@@ -1,15 +1,15 @@
 # sh curl-scripts/json/sign-up.sh
 
 # don't use a password you use for any real websites!
-curl "http://tic-tac-toe.wdibos.com/sign-up" \
+curl "https://tic-tac-toe-wdi.herokuapp.com/" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "'${EMAIL}'",
-      "password": "'${PASSWORD}'",
-      "password_confirmation": "'${PASSWORD}'"
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
     }
   }'
 
